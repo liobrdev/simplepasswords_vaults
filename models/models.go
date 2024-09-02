30394a5f048a14b1e0fc5ea66ed454187ffb8407
liobrdev <contact@liobr.dev>
2024-09-02 17:11:42 -0400

    Fixes foreign key constraint on delete cascade in model relations.

--- models/models.go
+++ models/models.go
@@ -6,9 +6,9 @@ type User struct {
 	Slug      string    `json:"user_slug" gorm:"primaryKey;not null"`
 	CreatedAt time.Time `json:"-" gorm:"autoCreateTime:nano;not null"`
 	UpdatedAt time.Time `json:"-" gorm:"autoUpdateTime:nano;not null"`
-	Vaults    []Vault   `json:"-" gorm:"foreignKey:UserSlug;references:Slug;not null"`
-	Entries   []Entry   `json:"-" gorm:"foreignKey:UserSlug;references:Slug;not null"`
-	Secrets   []Secret  `json:"-" gorm:"foreignKey:UserSlug;references:Slug;not null"`
+	Vaults    []Vault   `json:"-" gorm:"foreignKey:UserSlug;references:Slug;constraint:OnDelete:CASCADE"`
+	Entries   []Entry   `json:"-" gorm:"foreignKey:UserSlug;references:Slug"`
+	Secrets   []Secret  `json:"-" gorm:"foreignKey:UserSlug;references:Slug"`
 }
 
 type Vault struct {
@@ -17,8 +17,8 @@ type Vault struct {
 	UpdatedAt time.Time `json:"vault_updated_at" gorm:"autoUpdateTime:nano;not null"`
 	Title     string    `json:"vault_title" gorm:"uniqueIndex:unique_title_user_slug;not null"`
 	UserSlug  string    `json:"-" gorm:"uniqueIndex:unique_title_user_slug;index;not null"`
-	User      User      `json:"-" gorm:"foreignKey:UserSlug;constraint:OnDelete:CASCADE"`
-	Entries   []Entry   `json:"entries" gorm:"foreignKey:VaultSlug;references:Slug;not null"`
+	User      User      `json:"-" gorm:"foreignKey:UserSlug"`
+	Entries   []Entry   `json:"entries" gorm:"foreignKey:VaultSlug;references:Slug;constraint:OnDelete:CASCADE"`
 }
 
 type Entry struct {
@@ -27,9 +27,9 @@ type Entry struct {
 	UpdatedAt time.Time `json:"entry_updated_at" gorm:"autoUpdateTime:nano;not null"`
 	Title     string    `json:"entry_title" gorm:"uniqueIndex:unique_title_vault_slug;not null"`
 	VaultSlug string    `json:"-" gorm:"uniqueIndex:unique_title_vault_slug;index;not null"`
-	Vault     Vault     `json:"-" gorm:"foreignKey:VaultSlug;constraint:OnDelete:CASCADE"`
+	Vault     Vault     `json:"-" gorm:"foreignKey:VaultSlug"`
 	UserSlug  string    `json:"-" gorm:"not null"`
-	Secrets   []Secret  `json:"secrets" gorm:"foreignKey:EntrySlug;references:Slug;not null"`
+	Secrets   []Secret  `json:"secrets" gorm:"foreignKey:EntrySlug;references:Slug;constraint:OnDelete:CASCADE"`
 }
 
 func (Entry) TableName() string {
@@ -44,7 +44,7 @@ type Secret struct {
 	String    string    `json:"secret_string" gorm:"not null"`
 	Priority	uint8			`json:"secret_priority" gorm:"not null"`
 	EntrySlug string    `json:"-" gorm:"uniqueIndex:unique_label_entry_slug;index;not null"`
-	Entry     Entry     `json:"-" gorm:"foreignKey:EntrySlug;constraint:OnDelete:CASCADE"`
+	Entry     Entry     `json:"-" gorm:"foreignKey:EntrySlug"`
 	VaultSlug string    `json:"-" gorm:"not null"`
 	UserSlug  string    `json:"-" gorm:"not null"`
 }
